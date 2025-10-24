e128da05fcd2633a8647a648b4108f30b8260d51
sahadheep <sahadheepreddy@gmail.com>
2025-10-24 18:10:12 +0530

    Update index.js

--- final project/server/index.js
+++ final project/server/index.js
@@ -26,8 +26,9 @@ if (process.env.NODE_ENV !== 'test') {
 
 // Middleware
 app.use(helmet());
+const allowedOrigin = process.env.CLIENT_URL;
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: [allowedOrigin, 'http://localhost:3000', 'http://127.0.0.1:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
